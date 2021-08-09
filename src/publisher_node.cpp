c5f265245c9d27c0d7dc3ced43f6759764e16bdc
Hakan Köse <hakan.koese@alumni.fh-aachen.de>
2021-08-09 11:45:42 +0200

    Version mit Fehler im Run

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -2,32 +2,50 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "tutorial_interfaces/msg/Myobject.hpp"                                            // CHANGE
+
+#include "std_msgs/msg/string.hpp"
+
+//So funktioniert include
+#include "adp_core_msgs_test/msg/objectstamped.hpp"
+
+//funktioniert nicht, weil ObjectStamped
+//#include "adp_core_msgs/msg/objectStamped.hpp"
+using namespace std::chrono_literals;
 
 using namespace std::chrono_literals;
 
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
 class MinimalPublisher : public rclcpp::Node
 {
-public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
-  {
-    publisher_ = this->create_publisher<tutorial_interfaces::msg::Num>("topic", 10);  // CHANGE
-    timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-  }
+  public:
+    MinimalPublisher()
+    : Node("minimal_publisher"), count_(0)
+    {
+      
+      publisher_ = this->create_publisher<adp_core_msgs_test::msg::Objectstamped>("meineStringsAlda", 10);
+      
+    }
 
-private:
-  void timer_callback()
-  {
-    auto message = tutorial_interfaces::msg::Num();                                   // CHANGE
-    message.num = this->count_++;                                                     // CHANGE
-    RCLCPP_INFO_STREAM(this->get_logger(), "Publishing: '" << message.num << "'");    // CHANGE
-    publisher_->publish(message);
-  }
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<tutorial_interfaces::msg::Num>::SharedPtr publisher_;             // CHANGE
-  size_t count_;
+
+
+  private:
+    rclcpp::TimerBase::SharedPtr timer_;
+   
+
+    std::shared_ptr<rclcpp::Publisher<adp_core_msgs_test::msg::Objectstamped>> publisher_;
+    size_t count_;
+  
+    void timer_callback()
+    {
+      //auto test=adp_core_msgs::ObjectStamped();
+      auto message= adp_core_msgs_test::msg::Objectstamped();
+      message.motion_model=2;
+      RCLCPP_INFO(this->get_logger(), "I am Logging");
+      publisher_->publish(message);
+    }
+    
 };
 
 int main(int argc, char * argv[])
