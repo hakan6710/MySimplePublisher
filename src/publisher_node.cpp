6985b91f70293a0df9cdd851d3ff42cc4d4649d6
Hakan Köse <hakan.koese@alumni.fh-aachen.de>
2021-09-07 11:58:10 +0200

    Delete Tutorial Interfaces

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -37,7 +37,9 @@ class MinimalPublisher : public rclcpp::Node
       
       publisher_ = this->create_publisher<adp_core_msgs::msg::ObjectArrayStamped>("meineStrings", 10);
       pub_jpg = this->create_publisher<sensor_msgs::msg::CompressedImage>("meineBilder", 100);
-      loop();
+     
+
+      loop_cap();
     }
 
 
@@ -50,79 +52,8 @@ class MinimalPublisher : public rclcpp::Node
     size_t count_;
 
     std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::CompressedImage>> pub_jpg;
-    
 
-    // void timer_callback_temp()
-    // {
-      
-    //   adp_core_msgs::msg::ObjectArrayStamped message;
-    //   adp_core_msgs::msg::ObjectStamped objectStamped;
-    //   int numberOfObjects=0;
-
-    //   //ObjectStamped
-    //   int sensorIndex=1;
-    //   int objectIndex=1;
-    //   int motion_model=1;
-    //   adp_core_msgs::msg::MeasurementVector measurementVector;
-    //   measurementVector.x=0;
-    //   measurementVector.y=0;
-    //   measurementVector.z=0;
-
-    //   measurementVector.vx=0;
-    //   measurementVector.vy=0;
-    //   measurementVector.vz=0;
-
-    //   measurementVector.ax=1;
-    //   measurementVector.ay=1;
-    //   measurementVector.az=1;
-    //   measurementVector.orientation=0;
-      
-    //   adp_core_msgs::msg::MeasurementNoise measurementNoise;
-
-    //   measurementNoise.x_rms=1;
-    //   measurementNoise.y_rms=1;
-    //   measurementNoise.z_rms=1;
-    //   measurementNoise.vx_rms=1;
-    //   measurementNoise.vy_rms=1;
-    //   measurementNoise.vz_rms=1;
-    //   measurementNoise.ax_rms=1;
-    //   measurementNoise.ay_rms=1;
-    //   measurementNoise.az_rms=1;
-    //   measurementNoise.orientation_rms=1;
-
-
-
-    //   adp_core_msgs::msg::MeasurementParameters measurementParameters;
-    //   measurementParameters.origin_x=1;
-    //   measurementParameters.origin_y=1;
-    //   measurementParameters.origin_z=1;
-    //   measurementParameters.orientation_x=1;
-    //   measurementParameters.orientation_y=1;
-    //   measurementParameters.orientation_z=1;
-    //   measurementParameters.frame=1;
-
-
-    //   adp_core_msgs::msg::ObjectAttributes objectAttributes;
-    //   objectAttributes.dynamic_type=1;
-    //   objectAttributes.object_class=1;
-    //   objectAttributes.object_class_confidence=1;
-    //   objectAttributes.object_length=1;
-    //   objectAttributes.object_width=1;
-
-      
-    //   objectStamped.measurement_noise=measurementNoise;
-    //   objectStamped.measurement_parameters=measurementParameters;
-    //   objectStamped.measurement_vector=measurementVector;
-    //   objectStamped.object_attributes=objectAttributes;
-
-    //   message.objects[0]=objectStamped;
-      
-     
-    //   RCLCPP_INFO(this->get_logger(), "I am Logging");
-    //   publisher_->publish(message);
-    // }
-
-     map<int, string> getFilePathes(string directory)
+    map<int, string> getFilePathes(string directory)
     {
         map<int, string> map1;
         int number_of_zeros=6;
@@ -150,6 +81,52 @@ class MinimalPublisher : public rclcpp::Node
 
         return map1;
     }
+    cv::VideoCapture *cap;
+
+    cv::Mat getNextFrame()
+    {
+      cv::Mat frame;
+      *cap>>frame;
+
+      return frame;	
+
+    }
+
+    void InitCap(){
+      cap=new cv::VideoCapture("/home/fze2/Desktop/mot_files/test_data/aufnahme_Fahrt_Aachen.h264");
+      if(!cap->isOpened()){
+        throw std::runtime_error("VideoCap broken!"); 
+      }
+      cout<<"hi";
+
+    }
+
+    void loop_cap(){
+      cv::Mat currentFrame;
+
+      InitCap();
+      while(!(currentFrame=getNextFrame()).empty()){
+         //Führt zu seiner Exception. Nicht der gewünschte Weg aber das gewünschte Ergebnis.
+        if(!rclcpp::ok()){
+            cout<<"breche ab";
+            break;
+        }
+        
+        sensor_msgs::msg::CompressedImage image;  
+        int colorReadCode=1;
+        std_msgs::msg::Header myHeader;
+        cv_bridge::CvImage myImage(myHeader,"bgr8",currentFrame);
+        myImage.toCompressedImageMsg(image);
+
+      
+        image.header.stamp = this->now();
+        image.header.frame_id = "video";
+
+        pub_jpg->publish(image);
+        RCLCPP_INFO(get_logger(), "Publishing and Doing shit.!");
+        std::this_thread::sleep_for(0.1s);
+      }
+    }
     void loop()
     {
       
@@ -162,13 +139,18 @@ class MinimalPublisher : public rclcpp::Node
             break;
         }
         cv::Mat currentFrame;
-        sensor_msgs::msg::CompressedImage img_msg_compressed;  
+        sensor_msgs::msg::CompressedImage image;  
         int colorReadCode=1;
-        currentFrame = cv::imread(i2.second, colorReadCode);
+        currentFrame = cv::imread("/home/fze2/Desktop/target_89-1.png", colorReadCode);
         std_msgs::msg::Header myHeader;
         cv_bridge::CvImage myImage(myHeader,"bgr8",currentFrame);
-        myImage.toCompressedImageMsg(img_msg_compressed);
-        pub_jpg->publish(img_msg_compressed);
+        myImage.toCompressedImageMsg(image);
+
+      
+        image.header.stamp = this->now();
+        image.header.frame_id = "video";
+
+        pub_jpg->publish(image);
         RCLCPP_INFO(get_logger(), "Publishing and Waiting for 1sec now.!");
         std::this_thread::sleep_for(1s);
 
@@ -176,9 +158,9 @@ class MinimalPublisher : public rclcpp::Node
     }
 
 
-    adp_core_msgs::msg::ObjectArrayStamped getADP(){
+    // adp_core_msgs::msg::ObjectArrayStamped getADP(){
 
-    }
+    // }
     
 };
 
